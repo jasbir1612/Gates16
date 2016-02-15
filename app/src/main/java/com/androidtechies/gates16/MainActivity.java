d174e40074ff89b486064484c3eff078cad39b38
jasbir1612 <jasbir1612@gmail.com>
2016-02-15 20:38:51 +0530

    first commit

---
+++ app/src/main/java/com/androidtechies/gates16/MainActivity.java
@@ -0,0 +1,21 @@
+package com.androidtechies.gates16;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+    }
+}
